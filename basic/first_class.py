b7ad995b05a0a286f3a35d005b97eec2e2130fc1
Clive <clive.jan@gmail.com>
2020-01-15 14:58:30 -0500

    Add: defining a class and instantiating objects

---
+++ basic/first_class.py
@@ -0,0 +1,11 @@
+# Define a class
+class FirstClass:
+	pass
+
+# Instantiate objects from the defined class
+a = FirstClass()
+b = FirstClass()
+
+# Verify objects have been created from the defined class
+print(a)
+print(b)
