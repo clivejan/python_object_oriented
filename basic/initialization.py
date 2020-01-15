073b8567edacfb7d8e5ec0d7ddb28870c419d46a
Clive <clive.jan@gmail.com>
2020-01-15 16:34:56 -0500

    Add: using initializer to initialize attributes

---
+++ basic/initialization.py
@@ -0,0 +1,18 @@
+class Point:
+
+	def __init__(self, x, y):
+		"""using initializer to initialize object's attributes"""
+		self.move(x, y)
+
+	def move(self, x, y):
+		"""setting x, y coordinates by arguments"""
+		self.x = x
+		self.y = y
+
+	def reset(self):
+		"""setting x, y coordinates to zero"""
+		self.move(0, 0)
+
+# constructing a Point
+point = Point(3, 5)
+print(point.x, point.y)
