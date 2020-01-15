acf5f65d12abda1cd38e0b22c4fa8d8a47c2618e
Clive <clive.jan@gmail.com>
2020-01-15 15:06:32 -0500

    Add: setting arbitrary attributes on instantiated objects

---
+++ basic/object_attributes.py
@@ -0,0 +1,18 @@
+# Define a class
+class Point:
+	pass
+
+# Instantiate objects from the defined class
+p1 = Point()
+p2 = Point()
+
+# setting arbitrary attributes on instantiated objects
+p1.x = 5
+p1.y = 4
+
+p2.y = 3
+p2.y = 6
+
+# accseeing the attribute values
+print(p1.x, p1.y)
+print(p2.y, p2.y)
