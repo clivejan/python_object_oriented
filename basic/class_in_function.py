8bd7b3b27c2aa7edc8b4b9ac2bc7b80efe32fce8
Clive <clive.jan@gmail.com>
2020-01-15 20:50:39 -0500

    Add: classes can be defined in a function

---
+++ basic/class_in_function.py
@@ -0,0 +1,21 @@
+def format_string(string, formatter=None):
+	"""
+	Format a string using the formatter object, which
+	is expected to have a format() method that accepts
+	a string.
+	"""
+
+	class DefaultFormatter:
+		"""Format a string in title case."""
+
+		def format(self, string):
+			return str(string).title()
+
+	if not formatter:
+		formatter = DefaultFormatter()
+
+	return formatter.format(string)
+
+hello_string = 'hello world, how are you today?'
+print(f" Input: {hello_string}")
+print(f"Output: {format_string(hello_string)}")
