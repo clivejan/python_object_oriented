00654e5a64e2c18fc69c5952e98aeaf123d86c9b
Clive <clive.jan@gmail.com>
2020-01-15 20:41:44 -0500

    Add: execute something only when a module running as a script

---
+++ basic/module_as_script.py
@@ -0,0 +1,12 @@
+class UserfulClass:
+	"""This class might be useful to other modules."""
+	pass
+
+def main():
+	"""Creates a useful class and does something with ot for our module."""
+	useful = UserfulClass()
+	print(useful)
+
+# when this module running as a script, execute main() function
+if __name__ == '__main__':
+	main()
