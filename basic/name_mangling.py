e5b2338d65588b1d87dac919e9f7b4c248862bd7
Clive <clive.jan@gmail.com>
2020-01-15 21:06:02 -0500

    Add: using name mangling to indicate internal resource

---
+++ basic/name_mangling.py
@@ -0,0 +1,21 @@
+class SecertString:
+	"""A not-at-all secure way to store a secret string."""
+
+	def __init__(self, plain_string, pass_phrase):
+		# using double undersocres to strong indicate privete resources
+		self.__plain_string = plain_string
+		self.__pass_phrase = pass_phrase
+
+	def decrypt(self, pass_phrase):
+		"""Only show the string if the pass_phrase is corrent."""
+		if pass_phrase == self.__pass_phrase:
+			return self.__plain_string
+		else:
+			return ""
+
+# following the indicator to using the class
+secret_string = SecertString('my_passwd', 'encrypted')
+print(secret_string.decrypt('encrypted'))
+
+# try to access privete resources in the class
+print(secret_string.__plain_string)
