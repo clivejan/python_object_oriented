f3492b03f6a10e95bad92a86f0be1483b1035748
Clive <clive.jan@gmail.com>
2020-01-16 19:35:32 -0500

    Update: exception will crash calling function

--- exceptions/crash_program.py
+++ exceptions/crash_program.py
@@ -4,4 +4,12 @@ def no_return():
 	print("This line will never executed")
 	return "I won't be returned"
 
-no_return()
+#no_return()
+
+def call_exceptor():
+	print("call_exception starts here...")
+	no_return()
+	print("an exception was raised...")
+	print("...so these lines don't run")
+
+call_exceptor()
