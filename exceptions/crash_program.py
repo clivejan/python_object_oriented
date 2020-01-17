fdb4563b260fea55a34ceb07163ba9c84086b16e
Clive <clive.jan@gmail.com>
2020-01-16 19:28:16 -0500

    Add: exception will crash program immediately

---
+++ exceptions/crash_program.py
@@ -0,0 +1,7 @@
+def no_return():
+	print("I am about to raise an exception")
+	raise Exception("This is always raised")
+	print("This line will never executed")
+	return "I won't be returned"
+
+no_return()
