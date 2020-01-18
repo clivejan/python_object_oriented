e6872329af65d472036f6e7c82ac7dd580b7f9d9
Clive <clive.jan@gmail.com>
2020-01-18 10:01:02 -0500

    Update: adding comparason ability to dataclass

--- data_strcutures/dataclass_default.py
+++ data_strcutures/dataclass_default.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(order=True)
 class StockDecorated:
 	name: str
 	current: float = 0.0
@@ -14,3 +14,7 @@ print(stock)
 # assgin values to constrcut a object
 stock2 = StockDecorated("FB", 177.46, high=178.67, low=175.79)
 print(stock2)
+
+# add comparasons after orderred
+print(stock > stock2)
+print(stock < stock2)
