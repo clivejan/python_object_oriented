bebc312d44cf68a55cba4eb6a9505d755a32d4b9
Clive <clive.jan@gmail.com>
2020-01-18 09:31:15 -0500

    Add: using namedtuple to named data and make it read only

--- data_strcutures/tuples.py
+++ data_strcutures/tuples.py
@@ -1,9 +1,11 @@
 import datetime
 
 def middle(stock, date):
+	# tuple unpacks
 	symbol, current, high, low = stock
 	return ((high + low) / 2, date)
 
+# using parenthses to separate tuple from other parameters
 mid_value, date = middle(("FB", 177.46, 178.67, 175.79), \
 	datetime.date(2020, 1, 18))
 
