53f095c26f015eee2a4cea0013675d35272bc779
Clive <clive.jan@gmail.com>
2020-01-18 09:41:59 -0500

    Update: dataclass provides equlity

--- data_strcutures/dataclass.py
+++ data_strcutures/dataclass.py
@@ -14,3 +14,8 @@ print(stock.current)
 
 stock.unexpected_attribute = 'allowed'
 print(stock.unexpected_attribute)
+
+# it provides equlity ability
+stock2 = s("FB", 177.46, high=178.67, low=175.79)
+stock2.current = 100.0
+print(stock == stock2)
