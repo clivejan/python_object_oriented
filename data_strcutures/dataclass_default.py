974c46620c0ad4a593dd4799f64f5a2c9af5c7d5
Clive <clive.jan@gmail.com>
2020-01-18 09:56:23 -0500

    Add: set defualt values in dataclass

---
+++ data_strcutures/dataclass_default.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class StockDecorated:
+	name: str
+	current: float = 0.0
+	high: float = 0.0
+	low: float = 0.0
+
+# using default value to constrcut a object
+stock = StockDecorated("FB")
+print(stock)
+
+# assgin values to constrcut a object
+stock2 = StockDecorated("FB", 177.46, high=178.67, low=175.79)
+print(stock2)
